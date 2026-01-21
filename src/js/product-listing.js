26c29fc593410082474410ddd6765113e63ad803
d0dge1003 <ignaciokarelearl@gmail.com>
2026-01-22 04:19:38 +0800

    fix: add null check for category/query parameters

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -6,14 +6,34 @@ import ProductList from "./ProductList.mjs";
 loadHeaderFooter();
 
 const category = getParam("category");
+const query = getParam("q");
 const dataSource = new ProductData();
 const listElement = document.querySelector(".product-list");
-const myList = new ProductList(category, dataSource, listElement);
 
-// Set the title to show the category name
-const titleElement = document.querySelector(".title");
-if (titleElement) {
-    titleElement.innerHTML = category.charAt(0).toUpperCase() + category.slice(1);
+// Use query as key if present, otherwise category
+const searchKey = query || category;
+
+// If no search key, show error
+if (!searchKey) {
+    const titleElement = document.querySelector(".title");
+    if (titleElement) {
+        titleElement.innerHTML = "No category or search term provided";
+    }
+    if (listElement) {
+        listElement.innerHTML = '<p>Please select a category or use the search bar.</p>';
+    }
+} else {
+    const myList = new ProductList(searchKey, dataSource, listElement);
+
+    // Set the title to show the category name
+    const titleElement = document.querySelector(".title");
+    if (titleElement) {
+        if (query) {
+            titleElement.innerHTML = `Search Results: ${query}`;
+        } else if (category) {
+            titleElement.innerHTML = category.charAt(0).toUpperCase() + category.slice(1);
+        }
+    }
+
+    myList.init();
 }
-
-myList.init();
